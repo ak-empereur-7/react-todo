04f738e2c291f92cae600f7ffe28844c5457a76a
ak-empereur-7 <41908463+ak-empereur-7@users.noreply.github.com>
2021-08-10 07:14:42 +0900

    delete-todos

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,13 @@ export const App = () => {
     setTodoText("");
   };
 
+  const onclickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    // splice(何番目の要素から, いくつ削除する)
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
   return (
     <>
       <div className="input-area">
@@ -36,14 +43,14 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               // Reactでは仮想DOMが裏で差分を見ているので、ループのときは
               // returnの親要素にkeyを設定する
               <div key={todo} className="list-row">
                 <li>{todo}</li>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onclickDelete(index)}>削除</button>
               </div>
             );
           })}
